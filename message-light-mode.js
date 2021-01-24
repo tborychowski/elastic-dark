931a6ca448e072bcf3ebb022979472e50ad05262
Tomasz Borychowski <tborychowski@gmail.com>
2021-01-24 21:45:51 +0000

    remember message-light-mode (fixes #5)

--- message-light-mode.js
+++ message-light-mode.js
@@ -3,9 +3,19 @@ function messageLightMode (enable) {
 	const btnDark = document.querySelector('.message-toggle-dark');
 	const stylesheet = document.querySelector('#stylesheet-dark');
 
+	if (!stylesheet || !btnLight || !btnDark) return;
+
 	btnLight.classList.toggle('hidden', enable);	// hide btnLight when it's light
 	btnDark.classList.toggle('hidden', !enable);	// hide btnDark when it's not light
 
 	if (enable) stylesheet.setAttribute('disabled', 'true');
 	else stylesheet.removeAttribute('disabled');
+
+	window.localStorage.setItem('message-light-mode', enable);
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+	if (!document.querySelector('#message-content')) return;
+	const enable = window.localStorage.getItem('message-light-mode');
+	requestAnimationFrame(() => messageLightMode(enable === 'true'));
+});
